94f0567c1188cb90cba2ac007a4f51c6e022b122
Aurelienpt <aurelpt@gmail.com>
2018-10-17 18:52:11 +0000

    new chages

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const colors = ["#FF0000","#E2571E","#FF7F00","#FFFF00","#00FF00","#96bf33","#0000FF","#4B0082","#8B00FF","#ffffff"];
+const colors = ["#FF0000", "#E2571E", "#FF7F00", "#FFFF00", "#00FF00", "#96bf33", "#0000FF", "#4B0082", "#8B00FF", "#ffffff"];
+const disparitus = document.getElementById("remove-like");
+console.log(disparitus)
 
 document.getElementById("title").addEventListener("click", () => rainbow("title"));
 
@@ -7,13 +9,22 @@ function rainbow(id) {
 
     const setColor = i => {
         document.getElementById(id).style.color = colors[i];
-        if (i > colors.length-1) {i = 0;}
-        else {i++;}
+        if (i > colors.length - 1) {
+            i = 0;
+        } else {
+            i++;
+        }
         setTimeout(() => setColor(i), 250);
     }
 
     setColor(i);
 
+}
+
+function hasClicked() {
+
+    const resp = "<p style='color: gray; font-style: italic;'>Votre avis a été pris en compte.</p>"
+    disparitus.innerHTML = resp;
 }
 
 // On va choper le best content
